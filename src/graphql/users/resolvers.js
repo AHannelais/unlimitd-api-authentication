65353b0b7fd625eb256e66ff6474b555d6294c1f
AHANNELAIS <antoine.hannelais@gmail.com>
2023-07-27 23:36:01 +0200

    ✨ get users list for admins

--- src/graphql/users/resolvers.js
+++ src/graphql/users/resolvers.js
@@ -1,11 +1,18 @@
 import { combineResolvers } from 'graphql-resolvers';
 
 import UserWorkflow from '../../workflows/users';
+import isAdmin from '../shared/resolvers/isAdmin';
 import isAuthenticated from '../shared/resolvers/isAuthenticated';
 
 const Query = {
   currentUser: async function getCurrentUser(parent, args, { currentUser }) {
     const combined = combineResolvers(isAuthenticated, () => UserWorkflow.getAccountInfoById(currentUser.id));
+
+    return combined(parent, args, { currentUser });
+  },
+
+  usersList: async function getUsersList(parent, args, { currentUser }) {
+    const combined = combineResolvers(isAdmin, () => UserWorkflow.getUsersList());
 
     return combined(parent, args, { currentUser });
   },
